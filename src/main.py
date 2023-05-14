7aa98afc11d13fa643967cbbc223a10b9bf013b5
FacerAin <ywsong.dev@kakao.com>
2023-05-15 08:42:54 +0900

    feat: add video upload to mino

--- src/main.py
+++ src/main.py
@@ -2,16 +2,22 @@ import uvicorn
 from fastapi import FastAPI
 
 from database.connection import Settings
-from routes.users import router
+from routes import users, videos
 
 app = FastAPI()
 settings = Settings()
-app.include_router(router, prefix="/user")
+app.include_router(users.router, prefix="/user")
+app.include_router(videos.router, prefix="/video")
 
 
 @app.on_event("startup")
 async def init_db():
     await settings.initialize_database()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello YongFLIX!"}
 
 
 if __name__ == "__main__":
--- src/routes/videos.py
+++ src/routes/videos.py
@@ -1,1 +1,48 @@
+import io
+import os
+from datetime import datetime
 
+from fastapi import APIRouter, UploadFile
+from minio import Minio
+
+from database.connection import Database
+from models.videos import Video
+
+router = APIRouter(tags=["Video"])
+Database = Database(Video)
+
+
+def upload_file(data):
+    print(os.getenv("MINIO_ACCESS_KEY"))
+    client = Minio(
+        "localhost:9000/",
+        access_key="oFaJsqWTypO0JVmP",
+        secret_key="Zi4LfJ7PBqituyiXPLGfOEnmpMRbyRMx",
+        secure=False,
+    )
+
+    bucket_name = "videos"
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
+    else:
+        print(f"Bucket {bucket_name} already exists")
+    date_time = datetime.now()
+
+    file_path = f"video-" + str(date_time) + ".mp4"
+    with io.BytesIO(data) as bin_data_io:
+        client.put_object(
+            bucket_name,
+            file_path,
+            bin_data_io,
+            length=len(data),
+            content_type="video/mp4",
+        )
+
+    return {"bucket_name": bucket_name, "file_path": file_path}
+
+
+@router.post("/upload")
+async def upload_video(video_file: UploadFile):
+    upload_file(video_file.file.read())
+    return {"file_name": video_file.filename}
